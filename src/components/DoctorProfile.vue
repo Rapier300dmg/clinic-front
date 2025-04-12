ef4acfccf8f5c1dcd99e55fbba53225437e45c79
Rapier300dmg <161053481+Rapier300dmg@users.noreply.github.com>
2025-04-12 19:38:42 +0500

    Add files via upload

---
+++ src/components/DoctorProfile.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="doctor-profile">
+    <h2>{{ doctor.name }}</h2>
+    <p>{{ doctor.specialty }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DoctorProfile',
+  props: {
+    doctor: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.doctor-profile {
+}
+</style>
---
+++ src/views/DoctorProfile.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Doctor Profile</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DoctorProfilePage'
+}
+</script>
